82a3bf1e3f71e21c8393f50c2aa52d3548185b52
Brian Roberto Gómez Martínez <132787816+Briany4717@users.noreply.github.com>
2026-04-26 20:06:09 -0600

    Profile page added. (#1)
    Co-authored-by: Brianyan4717 <132787816+Brianyan4717@users.noreply.github.com>

--- WhirlpoolPromptWeb/Models/User.cs
+++ WhirlpoolPromptWeb/Models/User.cs
@@ -1,10 +1,13 @@
 public class User
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string LastName { get; set; }
     public DateTime Birthday { get; set; }
     public int Coins { get; set; }
     public ProfilePhoto ProfilePhoto { get; set; }
+    public int LocalRanking { get; set; }
+    public int NationalRanking { get; set; }
 }
 
 public enum ProfilePhoto
