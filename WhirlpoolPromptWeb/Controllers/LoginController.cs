dfa8bf98541a8790e4b7aa147aea1f5748ab0c6a
SrPika <104371533+RichGN28@users.noreply.github.com>
2026-05-19 12:08:26 -0600

    Sesion desde checker funcionando

--- WhirlpoolPromptWeb/Controllers/LoginController.cs
+++ WhirlpoolPromptWeb/Controllers/LoginController.cs
@@ -22,7 +22,8 @@ public class LoginController : Controller
 
         if (userIsValid)
         {
-            return RedirectToAction("Index", "Home");
+            SetUserSession(1);
+            return RedirectToAction("Check", "Login");
         }
 
         // agregar error si no se encuentra en la base de datos
@@ -31,6 +32,46 @@ public class LoginController : Controller
         return View(model);
 
 
+    }
+
+    private bool SetUserSession(int userID)
+    {
+
+        UserSession user = getUserFromId(userID);
+
+        HttpContext.Session.SetInt32("UserId", user.UserId);
+        HttpContext.Session.SetString("Name", user.Name);
+        HttpContext.Session.SetString("ProfileAddr", user.ProfileAddr);
+        HttpContext.Session.SetInt32("Coins", user.Coins);
+
+        return true;
+
+    }
+
+    private UserSession getUserFromId(int id)
+    {
+        UserSession user = new UserSession();
+
+        /* TODO: Obtener el user de la base de datos */
+        user.UserId = id;
+        user.Name = "Yoshi";
+
+        user.Coins = 67;
+
+        user.ProfileAddr = GetProfileAddr(ProfilePhoto.yoshi);
+
+        return user;
+    }
+
+    private string GetProfileAddr(ProfilePhoto profile)
+    {
+        switch (profile)
+        {
+            case ProfilePhoto.mario:
+                return "mario-bros.png";
+            default:
+                return "mario-bros.png";
+        }
     }
 
     private bool UserAuthentication(string user, string password)
@@ -42,7 +83,20 @@ public class LoginController : Controller
     [HttpGet("check")]
     public IActionResult Check()
     {
-        return View();
+        if (!IsSessionActive()) return RedirectToAction("Login");
+
+        var model = new UserSession
+        {
+            UserId = HttpContext.Session.GetInt32("UserId") ?? 0,
+            Name = HttpContext.Session.GetString("Name") ?? string.Empty,
+            ProfileAddr = HttpContext.Session.GetString("ProfileAddr") ?? string.Empty,
+            Coins = HttpContext.Session.GetInt32("Coins") ?? 0
+        };
+
+
+        return View(model);
     }
+
+    private bool IsSessionActive() => HttpContext.Session.GetInt32("UserId") != null;
 
 }
--- WhirlpoolPromptWeb/Program.cs
+++ WhirlpoolPromptWeb/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.IdleTimeout = TimeSpan.FromSeconds(30);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
